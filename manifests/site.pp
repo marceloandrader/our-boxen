321ebca3bd1500df622f6bc79449ed72b5e02437
Marcelo Andrade <marcelo.andrade.r@gmail.com>
2014-11-04 13:05:22 -0500

    add php

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,16 +64,36 @@ node default {
   }
 
   # node versions
-  include nodejs::v0_6
-  include nodejs::v0_8
+  # include nodejs::v0_6
+  # include nodejs::v0_8
   include nodejs::v0_10
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
+  # ruby::version { '1.9.3': }
+  # ruby::version { '2.0.0': }
+  # ruby::version { '2.1.0': }
+  # ruby::version { '2.1.1': }
   ruby::version { '2.1.2': }
+
+  # Install a php version and set as the global default php
+  class { 'php::global':
+    version => '5.5.9'
+  }
+
+  # Install Composer globally on your PATH
+  include php::composer
+
+  php::extension::mcrypt { 'mcrypt for 5.5.9':
+    php => '5.5.9'
+  }
+
+  php::extension::memcached { 'memcached for 5.5.9':
+    php => '5.5.9'
+  }
+
+  php::extension::xdebug { 'xdebug for 5.5.9':
+    php => '5.5.9'
+  }
 
   # common, useful packages
   package {
